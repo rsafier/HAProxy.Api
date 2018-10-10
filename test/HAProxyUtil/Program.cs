8ed27207a07f9687b8191fdeb1e7330c6ad1b076
Richard Safier <richardj@safier.com>
2018-10-09 20:37:14 -0400

    add a few more functions to cli sample

--- test/HAProxyUtil/Program.cs
+++ test/HAProxyUtil/Program.cs
@@ -7,30 +7,58 @@ namespace HAProxyUtil
 {
     class Program
     {
+        /// <summary>
+        /// Outputs JSON object of request.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             JsConfig.DateHandler = DateHandler.ISO8601DateTime;
             if (args.Length < 2)
             {
-                Console.WriteLine("HAProxyUtil <host>:<port> <command>");
-                Console.WriteLine("command: errors,info,backend,stat");
+                Console.WriteLine("   HAProxyUtil <host>:<port> <command> [arg()]   ");
+                Console.WriteLine("------------------  COMMANDS  -------------------");
+                Console.WriteLine("errors");
+                Console.WriteLine("info");
+                Console.WriteLine("backend");
+                Console.WriteLine("stats");
+                Console.WriteLine("drain <backend> <server>");
+                Console.WriteLine("disable <backend> <server>");
+                Console.WriteLine("enable <backend> <server>");
+                Console.WriteLine("setweight <backend> <server> <weight>");
+                Console.WriteLine("sendcommand <command> <Y>");
                 Environment.Exit(0);
             }
             var hostData = args[0].Split(":");
             var client = new HaProxyClient(hostData[0], hostData[1].ConvertTo<int>());
-            switch(args[1].ToLower())
+            switch (args[1].ToLower())
             {
                 case "errors":
-                    Console.WriteLine(client.ShowErrors().ToJson());
+                    Console.Write(client.ShowErrors().ToJson());
                     break;
                 case "info":
-                    Console.WriteLine(client.ShowInfo().ToJson());
+                    Console.Write(client.ShowInfo().ToJson());
                     break;
                 case "backend":
-                    Console.WriteLine(client.ShowBackendServers().ToJson());
+                    Console.Write(client.ShowBackendServers().ToJson());
                     break;
-                case "stat":
-                    Console.WriteLine(client.ShowStat().ToJson());
+                case "stats":
+                    Console.Write(client.ShowStat().ToJson());
+                    break;
+                case "drain":
+                    Console.Write(client.DrainServer(args[2], args[3]).ToJson());
+                    break;
+                case "enable":
+                    Console.Write(client.EnableServer(args[2], args[3]).ToJson());
+                    break;
+                case "disable":
+                    Console.Write(client.DisableServer(args[2], args[3]).ToJson());
+                    break;
+                case "setweight":
+                    Console.Write(client.SetWeight(args[2], args[3], args[4].ConvertTo<int>()).ToJson());
+                    break;
+                case "sendcommand":
+                    Console.Write(client.SendCommand(args[2], args.Length == 4)); //anything in 4th position will count as true for returning raw string (you gotta parse that yourself)
                     break;
             }
         }
